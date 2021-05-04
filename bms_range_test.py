20fa8e768b138895bd80f86a6a3743da928a6414
aswinmath94 <49982968+aswinmath94@users.noreply.github.com>
2021-05-04 21:26:36 +0530

    Update bms_range_test.py

--- bms_range_test.py
+++ bms_range_test.py
@@ -2,18 +2,15 @@ import unittest
 import bms_monitoring_ranges
 
 
+class CurrentRangesTest(unittest.TestCase):
+    def test_pass(self):
+        self.assertTrue(bms_monitoring_ranges.check_if_valid_input([1,2,3,4,5,6,8,9, 15,18,22]) == True)
+        self.assertTrue(bms_monitoring_ranges.check_if_valid_input([]) == False)
+        self.assertTrue(bms_monitoring_ranges.get_continous_range_count([1, 2, 3, 4, 5, 6, 8, 9, 15, 18, 22]) == 'Success')
+        self.assertTrue(bms_monitoring_ranges.get_continous_range_count([]) == 'Invalid Input')
 
-
-class test_battery_current_ranges(unittest.TestCase):
-    def test_passing_input_validation(self):
-        self.assertTrue(bms_monitoring_ranges.current_ranges([]) == "Invalid Input list")
-
-
-    def test_passing(self):
-        self.assertTrue(bms_monitoring_ranges.current_ranges([1,2, 3, 4,6, 7, 10, 11, 12, 13,15,19,25]) == "Valid Input list")
-
-    def test_failing(self):
-        self.assertFalse(bms_monitoring_ranges.current_ranges([2,3, 5, 6, 8,9]) == "Invalid Input list")  # no continuous range,should give invalid
+    def test_pass(self):
+        self.assertFalse(bms_monitoring_ranges.get_continous_range_count([1, 2, 3, 4, 5, 6, 8, 9, 15, 18, 22]) == 'Invalid Input')
 
 
 if __name__ == '__main__':
