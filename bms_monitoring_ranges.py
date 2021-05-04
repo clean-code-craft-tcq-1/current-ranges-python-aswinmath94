f8da331bb7d7db6a6118a53f282f0189914b272b
aswinmath94 <49982968+aswinmath94@users.noreply.github.com>
2021-05-04 21:26:18 +0530

    Update bms_monitoring_ranges.py

--- bms_monitoring_ranges.py
+++ bms_monitoring_ranges.py
@@ -1,74 +1,31 @@
-def current_ranges(ranges):
-    if check_if_valid_input(ranges):
-        no_of_bins = 4
-        bin_parameter = bin_parameter_calculator(ranges,no_of_bins)
-        bin_range_list = bin_ranges_calculator(bin_parameter)
-        bin_list = range_binner(ranges,bin_parameter,bin_range_list)
-        get_output(bin_list)
-        return 'Valid Input list'
-    else:
-        return 'Invalid Input list'
-
-
-
 def check_if_valid_input(ranges):
     if len(ranges) > 0:
-        return 1
+        return True
     else:
-        return 0
+        return False
 
-def bin_parameter_calculator(ranges,no_of_bins):
-    maximum_value = sorted(ranges)[-1]
-    minimum_value = sorted(ranges)[0]
+def get_continous_range_count(list_of_nos):
+    if check_if_valid_input(list_of_nos):
+        sorted_list = sorted(list_of_nos)
+        range_list = []
+        continous_list = []
 
-    #Assume equal width binning
-    bin_width = (maximum_value-minimum_value)/no_of_bins
-    value_dict ={'bin_wdith':bin_width,'maximum':maximum_value,'minimum':minimum_value,'no_of_bins':no_of_bins}
-    return value_dict
+        i = 0
+        while (i+1 < len(sorted_list)):
+            if ((sorted_list[i+1] - sorted_list[i]) <= 1):
+                continous_list.append(sorted_list[i])
+            else:
+                continous_list.append(sorted_list[i])
+                range_list.append(continous_list)
+                continous_list = []
 
-def bin_ranges_calculator(bin_paramters):
-    range_list =[]
-    for i in range(0, bin_paramters['no_of_bins'] + 1):
-        range_list = range_list + [bin_paramters['minimum'] + bin_paramters['bin_wdith'] * i]
-    return range_list
+            i += 1
+        print_range_count(range_list)
+        return 'Success'
+    else:
+        return 'Invalid Input'
 
-def range_binner(ranges,bin_parameters,range_list):
-    bin = []
-
-    for i in range(0, bin_parameters['no_of_bins']):
-        temp = []
-        for j in ranges:
-            lower_limit_flag = lower_limit_check(j,range_list[i])
-            upper_limit_flag = upper_limit_check(j,range_list[i])
-            limit_flag = check_within_limit(lower_limit_flag,upper_limit_flag)
-            if limit_flag:
-                temp += [j]
-        bin += [temp]
-    return bin
-
-def lower_limit_check(number,lower_limit):
-    if number >= lower_limit:
-        return True
-    return False
-
-def upper_limit_check(number,upper_limit):
-    if number <= upper_limit:
-        return True
-    return False
-
-def check_within_limit(lower_limit_flag,upper_limit_flag):
-    if lower_limit_flag and upper_limit_flag:
-        return True
-    return False
-
-
-def get_output(bin_list):
-    print(bin_list)
-    print('Ranges','Readings')
+def print_range_count(bin_list):
+    print('Ranges', 'Readings')
     for bin in bin_list:
-        if len(bin) > 0:
-            reading = len(bin)
-            ranges = str(bin[0]) + '-' + str(bin[len(bin) - 1])
-            output = str(ranges) + ',' + str(reading)
-            print(output)
-    return output
+        print(str(bin[0]) + "-" + str(bin[-1]) + ", " + str(len(bin)))
